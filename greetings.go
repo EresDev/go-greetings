38c6d8c78c11457abf39e83be40c20d9ce5d4b29
EresDev <hi@eresdev.com>
2020-09-28 12:24:31 +0500

    Greeting for multiple people. for loop, slice, blank identifier.

--- greetings.go
+++ greetings.go
@@ -1,32 +1,48 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
-func Hello(name string) (string, error){
-	
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func Hello(name string) (string, error) {
+
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	
+
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
-func init(){
-	rand.Seed(time.Now().UnixNano())
-}
-
 func randomFormat() string {
 	formats := []string{
-		"Hello %v",
-		"Great to see you %v",
-		"Hey %v",
+		"Hello %v ",
+		"Great to see you %v ",
+		"Hey %v ",
 	}
-	
+
 	return formats[rand.Intn(len(formats))]
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"example.com/greetings"
 	"fmt"
 	"log"
-	"example.com/greetings"
 )
 
-func main(){
-	log.SetPrefix("greetings: ");
-	// log.SetFlags(0);
-	
-	
-	message, err := greetings.Hello("Ares");
-	
-	if err != nil{
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Ares", "John", "Captain"}
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
 		log.Fatal(err)
 	}
-	
-	fmt.Println(message);
+
+	fmt.Println(messages)
 }
